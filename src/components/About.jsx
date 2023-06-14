6061487731332b656853fb22b23994fb0e254d82
bubun-001 <b420032@iiit-bh.ac.in>
2023-06-14 23:11:09 +0530

    initial commit

---
+++ src/components/About.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-fit bg-gradient-to-b from-gray-800 to-black text-white'>
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+            <div className='pb-8 pt-20'>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+            </div>
+            <p className='text-xl mt-20'>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus nam laudantium repellat numquam delectus aperiam placeat! Dolore natus animi ducimus quaerat illo vero ad tenetur rerum sapiente impedit odio eum blanditiis, magnam veritatis quis facere hic illum accusantium placeat enim eos culpa? A mollitia laborum qui provident at repellendus repellat.
+            </p>
+
+            <br />
+            
+
+            <p className='text-xl'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit corporis necessitatibus recusandae nesciunt voluptas beatae ea dolore quos in, non adipisci architecto, asperiores mollitia natus dicta. Incidunt, porro. Alias, non sunt. Blanditiis sapiente, ullam, dicta sed aut sit iusto veritatis praesentium totam minima quia harum minus quae molestiae est. Consectetur?
+            </p>
+        </div>
+    </div>
+  )  
+}
+
+export default About
