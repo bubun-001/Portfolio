6991de62949c2d42e5e01ca9e8652e9b3eedc8f1
bubun-001 <b420032@iiit-bh.ac.in>
2023-06-15 00:53:05 +0530

    error correction

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -82,7 +82,7 @@ import tailwind from '../assets/tailwind.png';
 
             {
               techs.map(({id , src , title , style }) =>(
-                <div key={id} className={'shadow-md hover:scale-105 duration-500 py-2 rounded-lg' + ' ' + style}>
+                <div key={id}className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
                <img src={src} alt=''className='w-20 mx-auto' />
                <p className='mt-4'>{title}</p>
             </div>
